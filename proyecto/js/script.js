2c3a0b1b9ace797b430f92edf6b20ffb85a74e15
xitzsoft <bcristian1987@gmail.com>
2016-08-13 11:07:30 -0300

    Template-Prub
    Prueba

---
+++ proyecto/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	//Mi contenedor tendrá un alto igual al alto del navegador.
+	$("#fondo").css({"height":$(window).height() + "px"});
+
+	/*$("a").hover(function(id){
+		$("a").css({"color": "#1ac761"});
+		$("i").css({"color": "#1ac761"});
+	});*/
+});
